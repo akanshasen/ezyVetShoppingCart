3471581132a6cd560b13fa1914ca02ddf70c5d63
Akansha Sen <akanshasen@hotmail.com>
2021-08-12 22:42:53 +1200

    added cart, cartItem, product and index php classes

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "Product.php";
+require_once "Cart.php";
+require_once "CartItem.php";
+
+
+
+
+
+?>
--- shoppingCartClass.php
+++ shoppingCartClass.php
@@ -1,7 +1,46 @@
+<?php 
+
 class ShoppingCart{
+	
+	public $products = [
+				[ "name" => "Sledgehammer", "price" => 125.75 ],
+				[ "name" => "Axe", "price" => 190.50 ],
+				[ "name" => "Bandsaw", "price" => 562.131 ],
+				[ "name" => "Chisel", "price" => 12.9 ],
+				[ "name" => "Hacksaw", "price" => 18.45 ],
+];
 	public $items_inCart = []; 
 	public $items_quantity = 0;
-	public $product_name = "";
+	public $name;
+	public $price;
+	public $total = 0;
+	
+	
+	//add product to cart method
+	public function add($name, $items_quantity)
+	{
+		
+	}
+	
+	//View current products in cart
+	
+	//cart products listed in format; product name, price, quantity, total, remove
+	
+	//total must be tallied up
+	
+	//remove product method
+	
+	//adding existing product will only update product quantity 
+	
+	//prices displayed to 2d.p.
+	
+	//error checking for completed code
+	
+	
+	
+	
 	
 	
 }
+
+?>
