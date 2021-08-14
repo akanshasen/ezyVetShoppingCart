54e362df4ad5501582e79ec7e836413429594e05
Akansha Sen <akanshasen@hotmail.com>
2021-08-14 15:56:23 +1200

    added variables and appropriate getters/setters

--- CartItem.php
+++ CartItem.php
@@ -1,5 +1,35 @@
 <?php
+/*
+This class is associated with the specific item in the cart. It contains functions to increase/decrease
+the amount of item selected/deselected respectively.
+*/
 
-echo "Hellow World!!";
+class CartItem
+{
+    private Product $product;
+    private int $quantity;
 
-?>
+    public function __construct(\Product $product, $quantity)
+    {
+        $this->product = $product;
+        $this->quantity = $quantity;
+    }
+
+    public function getProduct()
+    {
+        return $this->product;
+    }
+    public function setProduct($product)
+    {
+        $this->product = $product;
+    }
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+    }
+
+}
